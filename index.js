c9b5ae69891bb63a78a4ec99d5f4d34006948a8b
Jeoston Araújo <jeostonjunior@gmail.com>
2022-10-21 18:42:52 -0300

    alterations

--- index.js
+++ index.js
@@ -1,47 +1,40 @@
 const express = require("express");
-const app = express();
 const fs = require("fs");
+const app = express();
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/video", function (req, res) {
-    // Ensure there is a range given for the video
+app.get("/video", (req, res) => {
     const range = req.headers.range;
     if (!range) {
         res.status(400).send("Requires Range header");
     }
 
-    // get video stats (about 61MB)
     const videoPath = "Mugiwaras.mp4";
     const videoSize = fs.statSync("Mugiwaras.mp4").size;
 
-    // Parse Range
-    // Example: "bytes=32324-"
-    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const CHUNK_SIZE = 10 ** 6;
     const start = Number(range.replace(/\D/g, ""));
     const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
 
-    // Create headers
-    const contentLength = end - start + 1;
+    const contentLenght = end - start + 1;
     const headers = {
         "Content-Range": `bytes ${start}-${end}/${videoSize}`,
         "Accept-Ranges": "bytes",
-        "Content-Length": contentLength,
+        "Content-Lenght": contentLenght,
         "Content-Type": "video/mp4",
     };
 
-    // HTTP Status 206 for Partial Content
     res.writeHead(206, headers);
+    console.log(headers);
 
-    // create video read stream for this particular chunk
     const videoStream = fs.createReadStream(videoPath, { start, end });
-
-    // Stream the video chunk to the client
     videoStream.pipe(res);
 });
 
-app.listen(8000, function () {
+app.listen(8000, () => {
     console.log("Listening on port 8000!");
+    console.log("OPEN IN http://localhost:8000");
 });
